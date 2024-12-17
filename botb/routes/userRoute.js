078a0ea8dfa2415d28594b9030df03ff2e448b51
R.Abisheik <137792859+Abi-de-jo@users.noreply.github.com>
2024-12-17 19:51:04 +0530

    Update userRoute.js

--- botb/routes/userRoute.js
+++ botb/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {allLikes, createUser, dislikes, getuser, getusers, likes} from "../controllers/userCntrl.js";
+import {allLikes, createUser, dislikes, getuser, getusers, likes,updateUserEmail} from "../controllers/userCntrl.js";
  
 const router = express.Router();
 
 
 
+router.put("/updateuser", updateUserEmail);
 
 router.post("/register", createUser );
 router.post("/likes/:id", likes);
