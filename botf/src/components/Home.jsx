4ff7a26d332b44f7714784eeb8938f27cd45a1c0
R.Abisheik <137792859+Abi-de-jo@users.noreply.github.com>
2024-12-17 23:30:47 +0530

    Update Home.jsx

--- botf/src/components/Home.jsx
+++ botf/src/components/Home.jsx
@@ -52,11 +52,11 @@ function Home() {
   console.log("User registered successfully:", response.data.email);
 
           if (response.data.message === "Admin") {
-            localStorage.setItem("email",data.email)
+            localStorage.setItem("email",response.data.email)
             localStorage.setItem("role", "admin");
           } else if (response.data.message === "Agent") {
             localStorage.setItem("role", "agent");
-            localStorage.setItem("email",data.email)
+            localStorage.setItem("email",response.data.email)
           } else {
             localStorage.setItem("role", "user");
           }
